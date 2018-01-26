e6557d48a6ebf651a6f91f59b351d116402ac98d
mkwatuha <34641821+mkwatuha@users.noreply.github.com>
2018-01-26 10:21:34 +0300

    Adding more daily report definition and metadata (#2)

---
+++ api/src/main/java/org/openmrs/module/aihdreports/metadata/NCDMetaData.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.aihdreports.metadata;
+
+
+import org.openmrs.Concept;
+import org.openmrs.EncounterType;
+import org.openmrs.PatientIdentifierType;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Metadata definitions for the NCD related reporting
+ */
+@Component("ncdMetadata")
+public class NCDMetaData extends Metadata  {
+
+
+}
