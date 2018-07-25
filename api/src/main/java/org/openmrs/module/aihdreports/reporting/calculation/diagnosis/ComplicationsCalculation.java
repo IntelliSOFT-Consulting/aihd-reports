3eb4ea7b404ab6be600cafedb07192273ed498e0
kwatuha <kwatuha@gmail.com>
2018-07-26 00:20:09 +0300

    Complited complications and adjusted the treament calcultions

--- api/src/main/java/org/openmrs/module/aihdreports/reporting/calculation/diagnosis/ComplicationsCalculation.java
+++ api/src/main/java/org/openmrs/module/aihdreports/reporting/calculation/diagnosis/ComplicationsCalculation.java
@@ -9,7 +9,9 @@ import org.openmrs.calculation.result.ListResult;
 import org.openmrs.calculation.result.SimpleResult;
 import org.openmrs.module.aihdreports.reporting.calculation.AbstractPatientCalculation;
 import org.openmrs.module.aihdreports.reporting.calculation.Calculations;
+import org.openmrs.module.aihdreports.reporting.calculation.DyslipidemiaCalculation;
 import org.openmrs.module.aihdreports.reporting.calculation.EmrCalculationUtils;
+import org.openmrs.module.aihdreports.reporting.calculation.ObesityCalculation;
 import org.openmrs.module.aihdreports.reporting.metadata.Dictionary;
 import org.openmrs.module.aihdreports.reporting.metadata.Metadata;
 import org.openmrs.module.aihdreports.reporting.utils.CalculationUtils;
@@ -17,6 +19,7 @@ import org.openmrs.module.aihdreports.reporting.utils.CalculationUtils;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ComplicationsCalculation  extends AbstractPatientCalculation {
 
@@ -27,6 +30,8 @@ public class ComplicationsCalculation  extends AbstractPatientCalculation {
         CalculationResultMap hiv = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_STATUS), cohort, context);
         CalculationResultMap tb = Calculations.lastObs(Dictionary.getConcept(Dictionary.TB_STATUS), cohort, context);
         CalculationResultMap complications = Calculations.allObs(Dictionary.getConcept(Dictionary.PROBLEM_ADDED), cohort, context);
+        Set<Integer> obese = CalculationUtils.patientsThatPass(calculate(new ObesityCalculation(), cohort, context));
+        Set<Integer> dyslipidemia = CalculationUtils.patientsThatPass(calculate(new DyslipidemiaCalculation(), cohort, context));
         for(Integer ptId: cohort){
             String value = "";
             Concept hivStatus = EmrCalculationUtils.codedObsResultForPatient(hiv, ptId);
@@ -81,20 +86,52 @@ public class ComplicationsCalculation  extends AbstractPatientCalculation {
                         else if(StringUtils.isEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.ERECTILE_DISFUNCTION))){
                             value = "i";
                         }
+
+                        if(StringUtils.isNotEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.GASTROPATHY))){
+                            value += ",j";
+                        }
+                        else if(StringUtils.isEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.GASTROPATHY))){
+                            value = "j";
+                        }
+                        if(StringUtils.isNotEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.CATARACTS))){
+                            value += ",k";
+                        }
+                        else if(StringUtils.isEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.CATARACTS))){
+                            value = "k";
+                        }
+                        if(StringUtils.isNotEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.DENTAL_COMPLICATIONS))){
+                            value += ",l";
+                        }
+                        else if(StringUtils.isEmpty(value) && obs.getValueCoded().equals(Dictionary.getConcept(Dictionary.DENTAL_COMPLICATIONS))){
+                            value = "l";
+                        }
+                        if(StringUtils.isNotEmpty(value) && dyslipidemia != null && dyslipidemia.contains(ptId)){
+                            value += ",m";
+                        }
+                        else if(StringUtils.isEmpty(value) && dyslipidemia != null && dyslipidemia.contains(ptId)){
+                            value = "m";
+                        }
+                        if(StringUtils.isNotEmpty(value) && obese != null && obese.contains(ptId)){
+                            value += ",n";
+                        }
+                        else if(StringUtils.isEmpty(value) && obese != null && obese.contains(ptId)){
+                            value = "n";
+                        }
+
                     }
                 }
             }
-            if(StringUtils.isNotEmpty(value) && hivStatus.equals(Dictionary.getConcept(Dictionary.HIV_POSTIVE))){
+            if(StringUtils.isNotEmpty(value) && hivStatus != null && hivStatus.equals(Dictionary.getConcept(Dictionary.HIV_POSTIVE))){
                 value += ",o";
             }
-            else if(StringUtils.isEmpty(value) && hivStatus.equals(Dictionary.getConcept(Dictionary.HIV_POSTIVE))){
+            else if(StringUtils.isEmpty(value) && hivStatus != null && hivStatus.equals(Dictionary.getConcept(Dictionary.HIV_POSTIVE))){
                 value = "o";
             }
 
-            if(StringUtils.isNotEmpty(value) && tbStatus.equals(Dictionary.getConcept(Dictionary.ON_TREATMENT))){
+            if(StringUtils.isNotEmpty(value) && tbStatus != null && tbStatus.equals(Dictionary.getConcept(Dictionary.ON_TREATMENT))){
                 value += ",o";
             }
-            else if(StringUtils.isEmpty(value) && tbStatus.equals(Dictionary.getConcept(Dictionary.ON_TREATMENT))){
+            else if(StringUtils.isEmpty(value) && tbStatus != null && tbStatus.equals(Dictionary.getConcept(Dictionary.ON_TREATMENT))){
                 value = "o";
             }
 
