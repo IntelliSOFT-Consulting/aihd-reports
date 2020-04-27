69ac58c9d9f594b012c7ea75e30b50c44983a6ac
Kelvin Murumba <kevmurumba@Kelvins-MacBook-Pro.local>
2020-04-27 19:18:31 +0300

    Working System usage report UI

--- api/src/main/java/org/openmrs/module/aihdreports/reporting/queries/Queries.java
+++ api/src/main/java/org/openmrs/module/aihdreports/reporting/queries/Queries.java
@@ -6,8 +6,22 @@ public class Queries {
      * Get patients who are deceased with the specified dates
      * @return String
      */
-    public static String getDeceasedPatientsWithBoundaries(int causeOfDeath) {
-        String query = "SELECT pa.patient_id FROM patient pa INNER JOIN person pe ON pa.patient_id=pe.person_id INNER JOIN encounter e ON e.patient_id=pa.patient_id WHERE e.location_id=:location AND pe.death_date BETWEEN :startDate AND :endDate AND pe.cause_of_death=%d";
+    public static String getDeceasedPatientsWithBoundaries(final int causeOfDeath) {
+        final String query = "SELECT pa.patient_id FROM patient pa INNER JOIN person pe ON pa.patient_id=pe.person_id INNER JOIN encounter e ON e.patient_id=pa.patient_id WHERE e.location_id=:location AND pe.death_date BETWEEN :startDate AND :endDate AND pe.cause_of_death=%d";
         return String.format(query, causeOfDeath);
+    }
+
+    /**
+     * Get patient list for the system usage report
+     * 
+     * @return
+     */
+    public static String getPatientsListForSystemUsageReport() {
+        final String query = "SELECT pn.person_id,pi.identifier,pn.given_name,pn.middle_name,pn.family_name, lo.name,pi.date_created, ob.value_coded"
+        + " FROM person_name pn JOIN patient_identifier pi ON pi.patient_id=pn.person_id"
+        + " JOIN location lo ON lo.location_id=pi.location_id JOIN obs ob ON ob.person_id=pn.person_id "
+        + " WHERE pi.identifier_type=4 AND pi.identifier NOT LIKE '00000-%'  ORDER BY lo.name ASC ";
+        
+        return query;
     }
 }
--- api/src/main/java/org/openmrs/module/aihdreports/reports/SystemUsageReport.java
+++ api/src/main/java/org/openmrs/module/aihdreports/reports/SystemUsageReport.java
@@ -2,6 +2,11 @@ package org.openmrs.module.aihdreports.reports;
 
 import java.util.Properties;
 
+import org.openmrs.module.aihdreports.reporting.queries.Queries;
+import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.stereotype.Component;
@@ -11,42 +16,38 @@ public class SystemUsageReport extends AIHDDataExportManager {
 
     @Override
     public String getUuid() {
-        // TODO Auto-generated method stub
         return "3976c5f0-8492-11ea-bb57-e3371ce88bc1";
     }
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
         return "System Usage Report";
     }
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
         return "Collects a summary of system usage";
     }
 
     @Override
     public ReportDefinition constructReportDefinition() {
-        // TODO Auto-generated method stub
         ReportDefinition reportDefinition = new ReportDefinition();
         reportDefinition.setUuid(getUuid());
         reportDefinition.setName(getName());
         reportDefinition.setDescription(getDescription());
+
+        reportDefinition.addDataSetDefinition("SUR", Mapped.mapStraightThrough(constructSystemUsageDataset()));
 
         return reportDefinition;
     }
 
     @Override
     public String getVersion() {
-        // TODO Auto-generated method stub
         return "0.1";
     }
 
     @Override
     public String getExcelDesignUuid() {
-        // TODO Auto-generated method stub
         return "545edb82-8492-11ea-bba6-d353472f76d7";
     }
 
@@ -54,10 +55,18 @@ public class SystemUsageReport extends AIHDDataExportManager {
     public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
         ReportDesign reportDesign =createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "systemUsageReport.xls");
         Properties properties = new Properties();
-        properties.put("repeatingSections", "sheet:1,row:7,dataset:SUR");
+        properties.put("repeatingSections", "sheet:1,row:5,dataset:SUR");
         properties.put("sortWeight", "5000");
         reportDesign.setProperties(properties);
         return reportDesign;
+    }
+
+    private DataSetDefinition constructSystemUsageDataset(){
+        SqlDataSetDefinition su = new SqlDataSetDefinition();
+        su.setName("SUR");
+        su.setSqlQuery(Queries.getPatientsListForSystemUsageReport());
+
+        return su;
     }
 
     
